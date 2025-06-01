5769e4036b1454694c618f4a05fde447bc49c4ed
giangcoder11 <134074776+giangcoder11@users.noreply.github.com>
2025-06-01 12:31:03 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -107,7 +107,7 @@ $(document).ready(function () {
 
   // show popup
   $("#yes").click(function () {
-    var audio = new Audio("sound/20250601_122355121.m4r");
+    var audio = new Audio("sound/tick.mp3");
     audio.play();
     Swal.fire({
       title: textConfig.text7,
